ca5a7e4de446291ef9c476ac6fc4ce9830d620de
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-06-28 22:47:55 +0100

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,1 +1,14 @@
-0x00-ES6_basic
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
