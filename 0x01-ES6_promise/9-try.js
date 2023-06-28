f96b153d7b3eca7d1b917867f2c431a7ddea16fb
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-06-28 23:41:10 +0100

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,12 @@
-0x01-ES6_promise
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
