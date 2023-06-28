86f0c4d596c6c99388c5f17704f35d18ddaf0957
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-06-28 23:39:00 +0100

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,14 @@
-0x01-ES6_promise
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+};
+
+export default asyncUploadUser;
