67089e8e104dfc01add2ad4f0a6970e8d07d4a81
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-06-28 22:44:20 +0100

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,1 +1,9 @@
-0x00-ES6_basic
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
