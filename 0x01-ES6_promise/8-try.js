3039282fb16b625aa9103111bf96ff55f260f98e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-06-28 23:40:54 +0100

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,7 @@
-0x01-ES6_promise
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
