cdb2e8e53fd0ec8929537ca2d8fd0f973bce3caa
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-06-28 22:39:54 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,13 @@
-0x00-ES6_basic
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
