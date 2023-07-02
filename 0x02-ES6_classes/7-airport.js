20e48f878f89ed814b2ac858c8281a022f8ac9eb
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-07-02 19:41:14 +0100

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,16 @@
-0x02-ES6_classes
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
