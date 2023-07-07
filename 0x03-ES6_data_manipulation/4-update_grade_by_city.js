34435d76d0c22bfeed16f869afe54175a401cb13
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-07-07 04:07:49 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,16 @@
-0x03. ES6 data manipulation
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  const students = listStudents.filter((student) => student.location === city);
+  for (const student of students) {
+    student.grade = 'N/A';
+  }
+  const newStudents = students.map((student) => {
+    const single = student;
+    for (const grade of newGrades) {
+      if (single.id === grade.studentId) {
+        single.grade = grade.grade;
+      }
+    }
+    return single;
+  });
+  return newStudents;
+}
