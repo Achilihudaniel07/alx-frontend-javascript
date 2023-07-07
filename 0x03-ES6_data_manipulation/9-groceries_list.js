dd942067bb3d297cb14f5d531aa9ace5700ccc5d
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-07-07 04:13:16 +0100

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-0x03. ES6 data manipulation
+export default function groceriesList() {
+  const items = new Map();
+  items.set('Apples', 10);
+  items.set('Tomatoes', 10);
+  items.set('Pasta', 1);
+  items.set('Rice', 1);
+  items.set('Banana', 5);
+
+  return items;
+}
