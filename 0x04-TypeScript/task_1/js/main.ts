b9156aed9182d01981e6519c9500f815b8c44f3e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-07-09 03:51:57 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,7 @@
-jss
+"use strict";
+exports.__esModule = true;
+exports.printTeacher = void 0;
+var printTeacher = function (firstName, lastName) {
+  return firstName[0] + ". " + lastName;
+};
+exports.printTeacher = printTeacher;
