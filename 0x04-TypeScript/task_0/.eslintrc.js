78415eabe95188523e68071ac87068b3a0a008e5
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-07-09 03:49:31 +0100

    Update .eslintrc.js

--- 0x04-TypeScript/task_0/.eslintrc.js
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -1,1 +1,12 @@
-jss
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
