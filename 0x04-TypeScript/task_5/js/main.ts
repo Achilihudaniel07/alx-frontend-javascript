c92d3219a67a943c1775c11087fa042cd6bd144e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-07-09 04:10:29 +0100

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,17 @@
-jss
+interface MajorCredits {
+  credits: number;
+  brand: "major";
+}
+
+interface MinorCredits {
+  credits: number;
+  brand: "minor";
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+  return subject1.credits + subject2.credits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+  return subject1.credits + subject2.credits;
+}
